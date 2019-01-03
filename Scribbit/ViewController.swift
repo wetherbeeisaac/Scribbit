90a885ed609af9206c07a930218214af65abd410
Oron Barash <methgr@gmail.com>
2019-01-03 12:25:39 -0500

    Initial Commit

---
+++ Scribbit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribbit
+//
+//  Created by Oron Barash (student LM) on 1/3/19.
+//  Copyright © 2019 Oron Barash (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
